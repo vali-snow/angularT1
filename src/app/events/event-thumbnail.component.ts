9c0336a91486b127390cdf53d415689b94dee6e7
Valentin Sarghi <valentin.sarghi@kantarconsulting.com>
2019-06-10 18:25:19 +0300

    finished the 3rd module

--- src/app/events/event-thumbnail.component.ts
+++ src/app/events/event-thumbnail.component.ts
@@ -4,6 +4,8 @@ import { Component, Input, Output, EventEmitter } from '@angular/core'
     selector: 'event-thumbnail',
     templateUrl: './event-thumbnail.component.html',
     styles: [`
+      .green { color: #003333 !important; }
+      .bold { font-weight: bold;  }
       .thumbnail { min-height: 240px; }
       .pad-left { margin-left: 10px; }
       .well div { color: #bbb;}
@@ -12,4 +14,9 @@ import { Component, Input, Output, EventEmitter } from '@angular/core'
 
 export class EventThumbnailComponent {
   @Input() event:any
+
+  getStartTimeClass() {
+    const isEarlyStart = this.event && this.event.time === '8:00 am'
+    return {green: isEarlyStart, bold: isEarlyStart}
+  }
 }
