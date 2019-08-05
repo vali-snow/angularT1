a41b581b5a6bfd255bfcb4bd7f2f6108693ffa48
Valentin Sarghi <valentin.sarghi@kantarconsulting.com>
2019-08-05 13:21:44 +0300

    c7 - validating templatebased forms

--- src/app/user/login.component.ts
+++ src/app/user/login.component.ts
@@ -4,20 +4,23 @@ import { AuthService } from './auth.service';
 
 
 @Component({
-  templateUrl: './login.component.html'
+  templateUrl: './login.component.html',
+  styles: [`
+    em { float:right; color:#E05C65; padding-left:10px; }
+  `]
 })
 
 export class LoginComponent {
-  username; password;
-  constructor(private authService:AuthService, private router: Router) {
-    
-  }
+  username; password; mouseoverLogin;
+
+  constructor(private authService:AuthService, private router: Router) {}
 
   login(formValues) {
     console.log(formValues);
     this.authService.loginUser(formValues.userNamme, formValues.password);
     this.router.navigate(['events']);
   }
+  
   cancel() {
     this.router.navigate(['events']);
   }
