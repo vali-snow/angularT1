15a9982574fce09f4b1dbd569fcafaff147b7532
Valentin Sarghi <valentin.sarghi@kantarconsulting.com>
2019-08-06 13:05:06 +0300

    c11 - angular injection token

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,13 +10,15 @@ import {
 
 import { EventsAppComponent } from './events-app.component';
 import { NavBarComponent } from './nav/navbar.component';
-import { ToastrService } from './common/toastr.service';
+import { TOASTR_TOKEN, Toastr } from './common/toastr.service';
 import { CollapsibleWellComponent } from './common/collapsible-well.component';
 import { appRoutes } from '../routes';
 import { Error404Component } from './errors/404.component';
 import { AuthService } from './user/auth.service';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { Session } from 'protractor';
+
+declare let toastr:Toastr;
 
 @NgModule({
   imports: [
@@ -40,7 +42,10 @@ import { Session } from 'protractor';
   ],
   providers: [
     EventService,
-    ToastrService,
+    {
+      provide: TOASTR_TOKEN,
+      useValue: toastr
+    },
     EventRouteActivator,
     EventListResolver,
     AuthService,
