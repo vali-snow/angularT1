04271e0085570e0569606b56f4ddbf6371043e6c
Valentin Sarghi <valentin.sarghi@kantarconsulting.com>
2019-08-06 11:29:20 +0300

    c10 - finished

--- src/app/events/event-details/session-list.component.ts
+++ src/app/events/event-details/session-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnChanges } from '@angular/core';
 import { ISession } from '../shared/index';
 
 @Component ({
@@ -6,6 +6,48 @@ import { ISession } from '../shared/index';
   templateUrl: './session-list.component.html'
 })
 
-export class SessionListComponent {
+export class SessionListComponent implements OnChanges {
   @Input() sessions: ISession[];
+  @Input() filterBy: string;
+  @Input() sortBy: string;
+  visibleSessions: ISession[] = [];
+
+  ngOnChanges() {
+    if (this.sessions) {
+      this.filterSessions(this.filterBy);
+      this.sortSessions(this.sortBy);
+    }
+  }
+
+  filterSessions(filter) {
+    if (filter === 'all') {
+      this.visibleSessions = this.sessions.slice(0);
+    } else {
+      this.visibleSessions = this.sessions.filter(session => {
+        return session.level.toLocaleLowerCase() === filter;
+      });
+    }
+  }
+
+  sortSessions(sort){
+    switch(this.sortBy) {
+      case 'name': this.visibleSessions.sort(sortByNameAsc);
+      case 'votes': this.visibleSessions.sort(sortByVotesDesc);
+      default: break;
+    }
+  }
+}
+
+function sortByNameAsc(s1: ISession, s2: ISession) {
+  if (s1.name > s2.name) {
+    return 1;
+  } else if (s1.name === s2.name) {
+    return 0;
+  } else {
+    return -1;
+  }
+}
+
+function sortByVotesDesc(s1: ISession, s2: ISession) {
+  return s2.voters.length - s1.voters.length;
 }
